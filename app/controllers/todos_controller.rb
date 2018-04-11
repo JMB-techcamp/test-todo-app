4ebc5f6bdb82003e1e4bc091a41e06292e8071e1
JMBereder <jeanmichelbot@gmail.com>
2018-04-11 18:17:07 +0900

    Modified ApplicationController.

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,6 +1,17 @@
 class TodosController < ApplicationController
 
+  def sort
+    todo = Todo.find(paramss[:todo_id])
+    todo.update(todo_params)
+    render nothing: true
+  end
+
   def index
   end
 
+  private
+
+  def todo_params
+    params.require(:todo).permit(:name, :row_order_position)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+
+  resources :todos do
+    put :sort
+  end
+
   get 'todos' => 'todos#index'
 end
